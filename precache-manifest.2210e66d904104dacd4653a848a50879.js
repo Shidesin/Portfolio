6ecab69574a0c1885d15f92fc42a01f6d85c3567
Shidesin <shidesin@gmail.com>
2020-10-02 18:44:09 +0300

    Updates

---
+++ precache-manifest.2210e66d904104dacd4653a848a50879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f734a8bb56c0daa40663f0db7f4a2216",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e0d5618f9067b3687684",
+    "url": "/Portfolio/static/css/main.09b1a172.chunk.css"
+  },
+  {
+    "revision": "75a3168844590df7cc67",
+    "url": "/Portfolio/static/js/2.eb5aa08e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.eb5aa08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d5618f9067b3687684",
+    "url": "/Portfolio/static/js/main.71c78695.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
